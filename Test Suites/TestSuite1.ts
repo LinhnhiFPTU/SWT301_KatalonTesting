338cc347eac4854cf880e61d8808854e178eb761
LinhnhiFPTU <joliebui2002@gmail.com>
2023-07-11 23:07:43 +0700

    first commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>83cfb24a-8498-48d6-8c9d-e9720eb30af2</testSuiteGuid>
+   <testCaseLink>
+      <guid>522be846-73b5-45bd-8344-59ccee6f47c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddToCartTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9e013e2-c1d1-432a-bcd8-691fce73bd60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ChangeQuantityProductInCartTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuite2.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a8108f07-6e5a-491f-80ed-6318b6602374</testSuiteGuid>
+   <testCaseLink>
+      <guid>26b6f20b-3dae-4f5c-ae22-6a8c776b9511</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculateTotalBeforeCheckOutTest_1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5de433ca-fa2c-492a-88f2-022d8ec13a55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculateTotalBeforeCheckOutTest_2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
